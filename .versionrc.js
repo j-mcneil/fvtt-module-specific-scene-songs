b2a6296a40f34c4029c4860544e6e20e44332941
James McNeil <mcneil.james.r@gmail.com>
2021-03-28 18:43:30 -0400

    chore: make sure all generated artifacts are part of the release

--- .versionrc.js
+++ .versionrc.js
@@ -1,13 +1,14 @@
 module.exports = {
-  scripts :{
-    prerelease: "node prerelease.js",
-    prebump: "npm run build",
-    postbump: "node postbump.js"
+  scripts: {
+    prerelease: 'node prerelease.js',
+    prebump: 'npm run build',
+    postbump: 'node postbump.js',
+    precommit: 'git add -A',
   },
   bumpFiles: [
     {
-        filename: "package/module.json",
-        updater: "module-updater.js"
-    }
-  ]
+      filename: 'package/module.json',
+      updater: 'module-updater.js',
+    },
+  ],
 };
